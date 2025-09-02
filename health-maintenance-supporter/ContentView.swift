8484ceed72574c5a2a60ca6828118173d03c9c8d
Sridesh 001 <sridesh001@NIBMADMIN12s-iMac.local>
2025-09-02 12:13:45 +0530

    initil commit

---
+++ health-maintenance-supporter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  health-maintenance-supporter
+//
+//  Created by Sridesh 001 on 2025-09-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ health-maintenance-supporter/health_maintenance_supporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  health_maintenance_supporterApp.swift
+//  health-maintenance-supporter
+//
+//  Created by Sridesh 001 on 2025-09-02.
+//
+
+import SwiftUI
+
+@main
+struct health_maintenance_supporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
